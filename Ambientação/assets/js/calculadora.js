5593cc63abe261eae2bd000b52e76442b70f9a85
Wallis <wallis.sl@outlook.com>
2023-12-18 15:09:02 -0300

    up

---
+++ Ambientação/assets/js/calculadora.js
@@ -0,0 +1,15 @@
+console.log('Exercício js');
+
+//let clicou = document.getElementById("teste");
+
+
+/* clicou.addEventListener('click', validar);
+
+
+ function validar(){
+    console.log('Clicou');
+}  */
+
+function validar(){
+    console.log('Ola');
+}
