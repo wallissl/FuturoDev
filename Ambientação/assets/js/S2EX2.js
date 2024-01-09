9816836ce3ecd6aabf95dece25e28529e7eb1056
Wallis <wallis.sl@outlook.com>
2024-01-09 10:24:59 -0300

    S2EX2

--- Ambientação/assets/js/S2EX2.js
+++ Ambientação/assets/js/S2EX2.js
@@ -1,22 +1,35 @@
-// EXP
-//let clicou = document.getElementById("teste");
-
-
 /* clicou.addEventListener('click', validar);
-
 
  function validar(){
     console.log('Clicou');
-}  */
+}  // Chamar a função pelo javaScript */
+
 
 function validar(){
-
+    let operacao = document.getElementById('operacao');
+    let operacaoSelecionada = operacao.options[operacao.selectedIndex].value; //Pegar o valor inserido no select
+  
     let n1 = document.getElementById('n1').value;
     let n2 = document.getElementById('n2').value;
-    let soma = parseInt(n1) + parseInt(n2);
-    
-    console.log(soma)
 
-    document.getElementById('resposta').innerHTML = `O resultado da soma é ${soma}`;
-    
+    switch(operacaoSelecionada){
+        case 'soma':
+            let soma = parseInt(n1) + parseInt(n2);
+            document.getElementById('resposta').innerHTML = `O resultado da soma é: ${soma}`;
+            break;
+        case 'sub':
+            let sub = parseInt(n1) - parseInt(n2);
+            document.getElementById('resposta').innerHTML = `O resultado da subtração é: ${sub}`;
+            break;
+        case 'mult':
+            let mult = parseInt(n1) * parseInt(n2);
+            document.getElementById('resposta').innerHTML = `O resultado da multiplicação é: ${mult}`;
+            break;
+        case 'div':
+            let div = parseInt(n1) / parseInt(n2);
+            document.getElementById('resposta').innerHTML = `O resultado da divisão é: ${div}`;
+            break;
+        default:
+            alert("Informe um valor valido");
+    }
 }
