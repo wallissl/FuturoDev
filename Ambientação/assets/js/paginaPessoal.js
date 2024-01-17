da5cd7112c1026419a61e3147bac9c4edd1cee07
Wallis <wallis.sl@outlook.com>
2024-01-17 20:39:01 -0300

    :up

--- Ambientação/assets/js/paginaPessoal.js
+++ Ambientação/assets/js/paginaPessoal.js
@@ -1,1 +1,36 @@
-console.log('Hello World')
+console.log('Hello World');
+
+let artDes = document.getElementById('artDes');
+let artDev = document.getElementById('artDev');
+let header = document.getElementById('header');
+let linke = document.getElementsByClassName('linke');
+
+artDes.addEventListener('click', cliDes);
+artDev.addEventListener('click', cliDev);
+header.addEventListener('mousemove', mouHea);
+//linke.addEventListener('mousemove', linkeAcess);
+
+function cliDes(){
+    alert('Portfólio de Design | Behance');
+}
+
+function cliDev(){
+    alert('Repositório | GitHub ')
+}
+
+function clickCur(){
+    alert('Currículo | Linkedin')
+}
+
+function mouHea(){
+    header.style.backgroundColor="#6729ca";
+
+}
+
+header.addEventListener('mouseout', () =>{
+    header.style.backgroundColor="#000";
+});
+ 
+function linkeAcess(){
+    linke[0].style.color="#000";
+}
---
+++ domJs.md
@@ -0,0 +1,3 @@
+# domJs
+
+sdf
