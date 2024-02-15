97b975bbf177786c2eb32e9a3dee6c6a0e606b78
Wallis <wallis.sl@outlook.com>
2024-02-15 10:38:21 -0300

    :Exercícios M1S04

--- M1/Exercicios_S04/S04_Ex1/S04_Ex1.js
+++ M1/Exercicios_S04/S04_Ex1/S04_Ex1.js
@@ -1,4 +1,4 @@
-//  S04_Ex01: Crie uma função que exiba uma mensagem simples no console.
+// S04_Ex01: Crie uma função que exiba uma mensagem simples no console.
 
 function Hello(){
     console.log("Hello, World!");
--- M1/Exercicios_S04/S04_Ex2/S04_Ex2.js
+++ M1/Exercicios_S04/S04_Ex2/S04_Ex2.js
@@ -1,5 +1,5 @@
 // S04_Ex02: Declare uma variável e atribua um número a ela, em seguida, exiba seu valor no console. Atribua o valor a variável usando o window.prompt();
 
-let numero = window.prompt("Digite um valor número");
+let numero = window.prompt("Digite um valor de um número");
 
 console.log('O número digitado foi: ' + numero);
