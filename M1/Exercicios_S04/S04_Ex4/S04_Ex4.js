b26e0d19d473dad0c43eae192c999dde949afa10
Wallis <wallis.sl@outlook.com>
2024-02-15 13:25:29 -0300

    :Exercícios M1S04

---
+++ M1/Exercicios_S04/S04_Ex4/S04_Ex4.js
@@ -0,0 +1,8 @@
+// S04_Ex04: Crie uma função que receba dois números como parâmetros e retorne sua soma no console.log.
+
+function somarNumeros(n1,n2){
+    soma = (n1+n2)
+    console.log(soma);
+}
+
+somarNumeros(20,30);
