e2f4e8d55f7554d4769b9f42ae458acb575f24b9
Wallis <wallis.sl@outlook.com>
2024-03-05 23:08:38 -0300

    :up

--- M1/Exercicios_S05/S05_Ex01/S05_Ex1.js
+++ M1/Exercicios_S05/S05_Ex01/S05_Ex1.js
@@ -1,9 +1,15 @@
 // S05_Ex01: Desenvolva uma tela para simular consulta de preços e compra de produtos de mercado.S05_Ex01: Desenvolva uma tela para simular consulta de preços e compra de produtos de mercado.
 
-let codName = document.getElementById('codName').value;
+
 let resposta = document.getElementById('resposta')
 
 function valorProduto(){
+    let codName = document.getElementById('codName').value;
     resposta.innerHTML = (codName)
+
+    switch(codName){
+        case '201' || 'banana':
+            resposta.innerHTML = 'foi'
+    }
 }
 
