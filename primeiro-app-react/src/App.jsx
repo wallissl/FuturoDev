2dc610c69505b226ec5ba0d74e33f73738417bd3
Wallis <wallis.sl@outlook.com>
2024-03-25 22:00:56 -0300

    inicio react

---
+++ primeiro-app-react/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+
+function App() {
+  // bloco superir
+const nome = 'Wallis Silva'
+const email = 'email@emai.com'
+  
+  return (
+    // somente aqui aceita HTML
+    <div>
+      <h1>Nome do professor: {nome}</h1>
+      <h2>Email: {email}</h2>
+    </div>
+  )
+}
+
+export default App
