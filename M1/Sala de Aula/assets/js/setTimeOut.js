86cf28701cf121f827c5324a9501693a36084baa
Wallis <wallis.sl@outlook.com>
2024-04-02 13:20:16 -0300

    up

--- M1/Sala de Aula/assets/js/setTimeOut.js
+++ M1/Sala de Aula/assets/js/setTimeOut.js
@@ -1,3 +1,13 @@
-function tempo(){
-    setTimeout( () => window.alert("O tempo terminou"), 5000);
-}
+ function tempo(){
+    setTimeout( () => window.alert("O tempo terminou"), 1000);
+} // Executa uma ação após a contagem do tempo estipulado.
+
+const tempoInterval = setInterval(() => {
+    console.log('um segundo se passou')
+},1000) // Executa uma ação em loop toda vez que passa o tempo estipulado.
+
+clearInterval(tempoInterval) // Comando para limpar o intervalo
+
+/* JSON.parse(json) - Utilizado para converter JSON para Objeto.
+
+JSON.stringify(jsObject) - Utilizado para converter Objeto JavaScript para JSON. */
