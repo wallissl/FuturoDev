7f302c0f0f34c0bd6d8105b92f39950be7387802
Wallis <wallis.sl@outlook.com>
2024-06-12 17:09:34 -0300

    ex1

--- M2/Exercicios_S02/Ex1.js
+++ M2/Exercicios_S02/Ex1.js
@@ -1,1 +1,13 @@
-console.log('oi')
+let numeros = [1,2,3,4,5,6,7,8,9,10];
+   
+
+function filtrarPares(numeros){
+    
+    let numerosPares = numeros.filter((numero) => {
+        return numero %2 == 0;        
+    })
+
+    return numerosPares;
+}
+
+console.log(filtrarPares(numeros))
