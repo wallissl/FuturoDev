be32bceed326d9e1de246cd0e8222a53ef2c5a15
Wallis <wallis.sl@outlook.com>
2024-06-12 17:05:09 -0300

    ex1

--- M2/Exercicios_S01/Ex1.js
+++ M2/Exercicios_S01/Ex1.js
@@ -1,16 +1,15 @@
 
-let numeros = [1,2,3,4,5,6,7,8,9,10];
+ let numeros = [1,2,3,4,5,6,7,8,9,10];
    
 
 function filtrarPares(numeros){
     
     let numerosPares = numeros.filter((numero) => {
-        numero %2 == 0;
+        return numero %2 == 0;        
     })
-    return numerosPares
 
+    return numerosPares;
 }
 
-
-
 console.log(filtrarPares(numeros))
+
