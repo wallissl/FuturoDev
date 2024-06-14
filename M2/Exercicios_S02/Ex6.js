3bd982a793cf1dfdea733e4b908aa303447e328c
Wallis <wallis.sl@outlook.com>
2024-06-14 15:01:45 -0300

    ex6

---
+++ M2/Exercicios_S02/Ex6.js
@@ -0,0 +1,15 @@
+
+function transformarArray(array, transformacao){
+    const novoArray = array.map((item) => {
+        return transformacao(item) 
+    })
+    return novoArray
+}
+
+function dobrar(numero){
+    return numero *2
+}
+
+let numero = [1,2,3,4,5]
+
+console.log(transformarArray(numero, dobrar));
