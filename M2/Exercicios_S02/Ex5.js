74bd9f15ba1996633983427fb3ee22e10c7fa68e
Wallis <wallis.sl@outlook.com>
2024-06-16 14:15:08 -0300

    ex5

--- M2/Exercicios_S02/Ex5.js
+++ M2/Exercicios_S02/Ex5.js
@@ -1,14 +1,13 @@
 // Dado um array de números, crie uma função que use reduce para calcular o produto de todos os números no array.
 
 function calcularProduto(numeros) {
-        let produto = 1;
 
-        for(i=0; i< numeros.length; i++){
-            produto *= numeros[i]
-        }
-        return produto
+        let produto = numeros.reduce((total, numero) =>{
+            return total *= numero
+        },1)
+        return produto;
     }
     
     let numeros = [1, 2, 3, 4, 5];
 
-    console.log(calcularProduto(numeros)); // 120
+    console.log(calcularProduto(numeros));
