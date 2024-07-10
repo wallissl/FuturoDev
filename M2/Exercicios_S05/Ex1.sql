2b11cf7e86c8ee7c08fe109b7f38d0737772eb2c
Wallis <wallis.sl@outlook.com>
2024-07-09 21:11:05 -0300

    Ex1

---
+++ M2/Exercicios_S05/Ex1.sql
@@ -0,0 +1,19 @@
+/* Ex1 - Crie uma tabela chamada categorias com as seguintes colunas: categoria_id (chave primária), nome_categoria (VARCHAR(50)).
+
+Adicione uma coluna categoria_id à tabela produtos e crie uma chave estrangeira para a tabela categorias.
+
+Exclua a tabela categorias. */
+
+create table categorias(
+	categoria_id serial primary key,
+	nome_da_categoria varchar(50)
+)
+
+alter table produtos
+add column categoria_id int;
+
+alter table produtos
+add constraint fk_categoria
+FOREIGN KEY (categoria_id) references categorias(categoria_id);
+
+drop table categorias
